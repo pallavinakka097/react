48f4d36e4dd4d402bf588c57eaa6b5277563e449
pallavinakka097 <pallavinakka098@gmail.com>
2023-02-01 17:03:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './style.css';
+import image from './image1.jpg'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='k'>
+    <div className="p">
+      <img className='q' src={image} alt="dog image" />
+      <p className='r'>PUPPY</p>
+      <p className='l'>A puppy is a juvenile dog. Some puppies can weigh 1–1.5 kg (2.2–3.3 lb),
+         while larger ones can weigh up to 7–11 kg (15–24 lb). </p>
+      <a className='g'>SHARE</a>
+      <a className='g'>LEARN MORE</a>
+    </div>
     </div>
   );
 }
